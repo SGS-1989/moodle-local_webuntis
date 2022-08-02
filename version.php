0bc5c67aaaf8fa3eec06148ee5828c7fb4f77d6c
SGS-1989 <100760792+SGS-1989@users.noreply.github.com>
2022-08-03 01:10:48 +0200

    Update version.php

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2022070800;
+$plugin->version  = 2022070801;
 $plugin->requires = 2018051700;
 $plugin->component = 'local_webuntis';
-$plugin->release = '1.6.1 (Build: 2022070800)';
+$plugin->release = '1.6.1 MNG (Build: 2022070801)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array();
